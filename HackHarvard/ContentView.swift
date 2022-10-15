1a4d2e668c1d0b3f42d88a5c3fe36737f6080270
Brayton-Lordianto <braylord30@gmail.com>
2022-10-15 19:40:51 -0400

    test

--- HackHarvard/ContentView.swift
+++ HackHarvard/ContentView.swift
@@ -6,7 +6,9 @@
 //
 
 import SwiftUI
+import Foundation
 import RealityKit
+import ARKit
 
 struct ContentView : View {
     var body: some View {
@@ -21,10 +23,25 @@ struct ARViewContainer: UIViewRepresentable {
         let arView = ARView(frame: .zero)
         
         // Load the "Box" scene from the "Experience" Reality File
-        let boxAnchor = try! Experience.loadBox()
-        
+//        let boxAnchor = try! Experience.loadBox()
+        let anchor = AnchorEntity()
+        let parentEntity = ModelEntity()
+        let entity = try! Entity.load(named: "jump1")
+        parentEntity.addChild(entity)
+        anchor.scale = [0.5,0.5,0.5]
+        anchor.addChild(parentEntity)
+        print("DEBUG: \(entity.availableAnimations.count)")
+        arView.scene.addAnchor(anchor)
+        for anim in entity.availableAnimations {
+            print("DEBUG: hi")
+            entity.playAnimation(anim.repeat())
+        }
+        let entityBounds = entity.visualBounds(relativeTo: parentEntity)
+        parentEntity.collision = CollisionComponent(shapes: [ShapeResource.generateBox(size: entityBounds.extents).offsetBy(translation: entityBounds.center)])
+
+        arView.installGestures(for: parentEntity)
+
         // Add the box anchor to the scene
-        arView.scene.anchors.append(boxAnchor)
         
         return arView
         
